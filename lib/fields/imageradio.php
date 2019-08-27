fce15e376668f1cbb77aeb2b3eb1b45a49e2dcb1
Bert Willekens <bert@willekens.email>
2019-08-27 11:31:38 +0200

    Hotfix for ratio issue caused by Kirby Builder

--- lib/fields/imageradio.php
+++ lib/fields/imageradio.php
@@ -3,50 +3,50 @@
 require_once dirname(__DIR__) . '/options/imageradio-optionsquery.php';
 require_once dirname(__DIR__) . '/options/imageradio-options.php';
 
-
 $base = require kirby()->root('kirby') . '/config/fields/radio.php';
-
 
 /* Merge new properties
 --------------------------------*/
 
 $base = array_merge_recursive($base, array(
     'props' => array(
-        'fit' => function($fit = 'cover') {
+        'fit' => function ($fit = 'cover') {
             return $fit;
         },
-        'gap' => function($gap = false) {
+        'gap' => function ($gap = false) {
             return $gap;
         },
-        'mobile' => function($mobile = false) {
+        'mobile' => function ($mobile = false) {
             return $mobile;
         },
-        'ratio' => function($ratio = '1/1') {
-        	$computed = 1;
+        'ratio' => function ($ratio = '1/1') {
+            if (is_numeric($ratio)) {
+                return $ratio;
+            }
 
-            if (preg_match('/(\d+)(?:\s*)([\/])(?:\s*)(\d+)/', $ratio, $matches) !== false){
+            $computed = 1;
+
+            if (preg_match('/(\d+)(?:\s*)([\/])(?:\s*)(\d+)/', $ratio, $matches) !== false) {
                 $computed = $matches[3] / $matches[1];
             }
-            
+
             $computed = $computed * 100;
-            $ratio    = round($computed, 3, PHP_ROUND_HALF_DOWN);
+            $ratio = round($computed, 3, PHP_ROUND_HALF_DOWN);
 
             return $ratio;
         },
     ),
 ));
 
-
 /* Replace existing properties
 --------------------------------*/
 
 $base = array_replace_recursive($base, array(
     'computed' => array(
-    	'options' => function() {
-    		return ImageRadioOptions::factory($this->options(), $this->props, $this->model());
+        'options' => function () {
+            return ImageRadioOptions::factory($this->options(), $this->props, $this->model());
         },
     ),
 ));
-
 
 return $base;
